b93180245721f73479e32691755d55fde8024578
Mahdi Vajdi <mahdivajdii@gmail.com>
2024-01-04 14:44:37 +0330

    refactor: chagne the directory of a few dtos

---
+++ libs/common/src/dto-command/auth/index.ts
@@ -0,0 +1,4 @@
+export * from './refresh-tokens.dto';
+export * from './signin.dto';
+export * from './signout.dto';
+export * from './signup.dto';
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,5 +2,6 @@ export * from './auth';
 export * from './constants';
 export * from './dto';
 export * from './dto-query';
+export * from './dto-command';
 export * from './decorators';
 export * from './pipes';
